924eddac04a1f8b74de1b5a9251c78d8f9dd7c4a
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-11-14 16:06:23 +0800

    Patch and Put example

--- WebApplicationDemo/Controllers/PutPatchExampleController.cs
+++ WebApplicationDemo/Controllers/PutPatchExampleController.cs
@@ -4,10 +4,44 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using static WebApplicationDemo.Controllers.PostExampleController;
 
 namespace WebApplicationDemo.Controllers
 {
     public class PutPatchExampleController : ApiController
     {
+        List<Student> studentsList = new List<Student>();
+
+        [HttpPatch]
+        public List<Student> PatchStudentName(int id, string name)
+        {
+            Student existStudent = studentsList.Where(x => x.id == id).FirstOrDefault();
+            if (existStudent == null)
+            {
+                return null;
+            }
+
+            studentsList.Remove(existStudent);
+            existStudent.name = name;
+            studentsList.Add(existStudent);
+
+            return studentsList;
+        }
+
+        [HttpPut]
+        public List<Student> PutStudentName(Student student)
+        {
+            Student existStudent = studentsList.Where(x => x.id == student.id).FirstOrDefault();
+            if (existStudent == null)
+            {
+                return null;
+            }
+
+            studentsList.Remove(existStudent);
+            studentsList.Add(existStudent);
+
+            return studentsList;
+        }
+
     }
 }
